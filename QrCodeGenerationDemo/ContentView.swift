e581fc66f9b06a713536c6ad02905b084ef78b9c
adarsh <adarsh0ranjan@gmail.com>
2023-02-01 16:28:22 +0530

    Initial Commit

---
+++ QrCodeGenerationDemo/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  QrCodeGenerationDemo
+//
+//  Created by Roro Solutions on 01/02/23.
+//
+
+import SwiftUI
+import CoreImage.CIFilterBuiltins
+
+struct ContentView: View {
+    @State private var name = "Adarsh"
+    @State private var emailAddress = "adarsh.ranjan@roro.io"
+        
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .resizable()
+                    .interpolation(.none)
+                    .scaledToFit()
+                    .frame(width: 300, height: 200)
+            }
+            .navigationTitle("Your code")
+        }
+    }
+    
+    // QR code generator
+    func generateQRCode(from string: String) -> UIImage {
+        
+        let context = CIContext()
+        let filter = CIFilter.qrCodeGenerator()
+        
+        filter.message = Data(string.utf8)
+
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QrCodeGenerationDemo/QrCodeGenerationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrCodeGenerationDemoApp.swift
+//  QrCodeGenerationDemo
+//
+//  Created by Roro Solutions on 01/02/23.
+//
+
+import SwiftUI
+
+@main
+struct QrCodeGenerationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
