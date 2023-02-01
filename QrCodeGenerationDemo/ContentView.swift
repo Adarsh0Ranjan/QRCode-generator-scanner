5015cb1d3b9460b675d7a0fc700380ed4e4d33e9
adarsh <adarsh0ranjan@gmail.com>
2023-02-01 18:33:50 +0530

    comments added

--- QrCodeGenerationDemo/ContentView.swift
+++ QrCodeGenerationDemo/ContentView.swift
@@ -36,17 +36,22 @@ struct ContentView: View {
     // QR code generator
     func generateQRCode(from string: String) -> UIImage {
         
+        //  properties to store an active Core Image context and an instance of Core Image’s QR code generator filter.
         let context = CIContext()
         let filter = CIFilter.qrCodeGenerator()
         
-        filter.message = Data(string.utf8)
+        // set input message for the QR code
+        filter.setValue(string.data(using: .utf8), forKey: "inputMessage")
+//        filter.message = Data(string.utf8)
 
+        // Get the output image from filter
         if let outputImage = filter.outputImage {
+
+            //  create cgimage and concert them to ui image
             if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
                 return UIImage(cgImage: cgimg)
             }
         }
-
         return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
